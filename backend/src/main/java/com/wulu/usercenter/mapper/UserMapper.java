faa50026aef36fdb4f9b44c68602bb9de1555a1a
wulu0120 <wulu0120@gmail.com>
2022-07-30 22:42:29 +0800

    delete user class

--- backend/src/main/java/com/wulu/usercenter/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wulu.usercenter.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wulu.usercenter.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
